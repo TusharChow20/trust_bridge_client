502589ddd7ab98e01c9cce9e13f934a2bad37f57
Tushar Chowdhury <tusharchowdhury20211@gmail.com>
2026-02-25 02:27:14 +0600

    navbar

---
+++ src/components/layout/Navbar.jsx
@@ -0,0 +1,64 @@
+import Image from "next/image";
+import React from "react";
+import logo from "@/../public/logo.png";
+import { Button } from "../ui/button";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Menu } from "lucide-react";
+export default function Navbar() {
+  const navLinks = [
+    {
+      label: "About Us",
+      href: "/about",
+    },
+    { label: "All Products", href: "/products" },
+    {
+      label: "Login",
+      href: "/login",
+    },
+  ];
+  return (
+    <div className="mt-5 mx-5 md:mx-1 flex justify-between items-center shadow-md rounded-md px-3">
+      <div className="w-20 md:w-80 ">
+        <Image
+          className="rounded-full"
+          src={logo}
+          alt="Logo"
+          width={60}
+          height={80}
+        ></Image>
+      </div>
+      <div className="hidden md:flex">
+        {navLinks.map((link, index) => (
+          <Button key={index} variant="ghost">
+            {link.label}
+          </Button>
+        ))}
+      </div>
+      <div className="md:hidden">
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline">
+              <Menu />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuGroup>
+              {navLinks.map((link, index) => (
+                <DropdownMenuItem key={index}>{link.label}</DropdownMenuItem>
+              ))}
+            </DropdownMenuGroup>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+    </div>
+  );
+}
